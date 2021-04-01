e8a94a6d786a71b06e21db1dfbcbec38c8750f0c
benseddoncode <ben.seddon@aussiebb.com.au>
2021-04-01 17:14:58 +1100

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let result
+  if (ride <= 400) {
+  result = "This one is on me!"}
+  else if (1999 < ride && ride < 2500) {
+    result = "I will gladly take your thirty bucks."
+  }
+  else {
+    result = "No can do."
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result
+  if (city === 'NYC') {
+    result = "Ok, sounds good."
+  }
+  else {
+    result = "No go."
+  }
+  return result
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
+
